6b7489c29946aa1cc416bdbdd739dbd7ce5e4864
Ed1 <edwanhernandez@gmail.com>
2023-10-02 23:43:32 -0700

    Add HTTP Client For Lambda Function

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const lambdaUrl = "https://v7exa47g23m3yy3chav3iclgme0egtjn.lambda-url.us-west-1.on.aws/";
+  const getMarketResearch = async () => {
+    const config = {
+      baseURL: lambdaUrl,
+    };
+    const response = await axios.get('/', config);
+    console.log(response);
+    return response;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code onClick={getMarketResearch}>src/App.js</code> and save to reload.
           THIS IS A TEST
         </p>
         <a
